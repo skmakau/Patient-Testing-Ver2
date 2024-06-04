14ef19f47de506779c0a8ac62f5d275a2a97a3a3
skmakau <103450393+skmakau@users.noreply.github.com>
2024-06-04 16:10:14 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById('surveyForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    fetch('https://script.google.com/macros/s/AKfycbyPrvPOfwSpiHGsCQWNPeWd6U86005LXJKTVdQiaAHezXKLRPDmkXcvm27Aed1fNAue/exec', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(data => {
+        if (data.includes('success')) {
+            alert('Thank you for your ratings!');
+            window.close();
+        } else {
+            throw new Error('Submission failed');
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert('There was an error submitting your ratings. Please try again.');
+    });
+});
